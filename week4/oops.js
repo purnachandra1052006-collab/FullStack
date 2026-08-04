873b671dac1b3853db39300e38366107def8749b
purnachandra1052006-collab <purnachandra1052006@gmail.com>
2026-08-04 10:04:56 +0530

    Add files via upload

---
+++ week4/oops.js
@@ -0,0 +1,44 @@
+// Single Inheritance
+class Person{
+    constructor(name, year) {
+    this.name = name;
+    this.year = year;
+    }
+
+    display(){
+        console.log(this.name);
+        console.log(this.year);
+    }
+
+    age() {
+        return 2026-this.year
+    }
+}
+
+const p1 = new Person("charan", "2006");
+p1.display();
+console.log(p1.age());
+
+class Student extends Person{
+    constructor(name,year,gpa,branch) {
+        super(name,year);
+        this.branch=branch;
+        this.gpa=gpa;
+    }
+
+    display(){
+        super.display();
+        console.log(this.gpa);
+        console.log(this.branch);
+    }
+}
+
+const s1=new Student("Purna","2006","9","CSM");
+s1.display();
+
+// Multi-Level Inheritance
+class GradStudent extends Student{
+    constructor(name,year,gpa,branch,hird_company) {
+        
+    }
+}
