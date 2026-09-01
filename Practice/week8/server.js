47b99b10b79c2a568b115e7a473303799bd14b64
purnachandra1052006-collab <purnachandra1052006@gmail.com>
2026-09-01 05:38:28 +0530

    Added .gitignore

---
+++ Practice/week8/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/home',(req,res)=>{
+    res.send("Home Page");
+});
+
+server.get('/about',(req,res)=>{
+    res.send("About Page");
+});
+
+server.get('/contact',(req,res)=>{
+    res.send("Contact me!")
+});
+server.listen(3000, ()=>{
+    console.log("The Server is working");
+});
--- Records/week_3/arrayfunctions.js
+++ Records/week_3/arrayfunctions.js
@@ -1,43 +1,62 @@
 // Arrays and Functions
 
-let list = [];
+let numbers = [];
 
-function addTask(task) {
-    list.push(task);
-    console.log(`Added: ${task}`);
+function addNumber(num) {
+    numbers.push(num);
+    console.log(`Added: ${num}`);
 }
 
-function showTasks() {
-    console.log("\nYour Tasks:");
-    if (list.length === 0) {
-        console.log("No tasks yet");
-        return;
-    }
-    for (let i = 0; i < list.length; i++) {
-        console.log(`${i+1}. ${list[i]}`);
-    }
+function displayAll() {
+    console.log("\nNumbers:", numbers.join(" | "));
 }
 
-function completeTask(index) {
-    if (index >= 0 && index < list.length) {
-        let done = list[index];
-        list.splice(index, 1);
-        console.log(`Completed: ${done}`);
+function getSum() {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    console.log(`Sum = ${sum}`);
+    return sum;
+}
+
+function getAverage() {
+    if (numbers.length === 0) return 0;
+    let sum = getSum();
+    let avg = sum / numbers.length;
+    console.log(`Average = ${avg.toFixed(2)}`);
+    return avg;
+}
+
+function removeNumber(num) {
+    let idx = numbers.indexOf(num);
+    if (idx !== -1) {
+        numbers.splice(idx, 1);
+        console.log(`Removed: ${num}`);
     } else {
-        console.log("Invalid task number");
+        console.log(`${num} not found`);
     }
 }
 
-function countTasks() {
-    console.log(`Total tasks: ${list.length}`);
+
+function clearAll() {
+    numbers = [];
+    console.log("Cleared all numbers");
 }
 
 
-addTask("Buy groceries");
-addTask("Walk the dog");
-addTask("Read a book");
-showTasks();
-countTasks();
-completeTask(1);
-showTasks();
-countTasks();
+addNumber(8);
+addNumber(3);
+addNumber(15);
+addNumber(6);
+addNumber(22);
+
+displayAll();
+getSum();
+getAverage();
+
+removeNumber(15);
+displayAll();
+
+clearAll();
+displayAll();
